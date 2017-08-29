a7308dbee8c4bc86833a57433ed42f8d01fa2e8a
kba239 <adams.kate.b@gmail.com>
2017-08-29 11:35:49 -0700

    Separate views for active and for past tasks

--- app/scripts/controllers/PastTasksCtrl.js
+++ app/scripts/controllers/PastTasksCtrl.js
@@ -1,10 +1,9 @@
 (function() {
   function PastTasksCtrl(Task) {
-    var ctrl = this;
 
-    ctrl.tasks = Task.all;
+    this.tasks = Task.all;
 
-    ctrl.expiredTask = function(timeDate) {
+    this.expiredTask = function(timeDate) {
       var sevenDays = 604740000;
       var currentTimeDate = new Date().getTime();
       if (currentTimeDate > (timeDate + sevenDays)) {
@@ -12,8 +11,8 @@
       }
     };
 
-    ctrl.completedTask = function (completedStatus) {
-      if (completedStatus === "yes") {
+    this.completedTask = function (completedStatus) {
+      if (completedStatus === true) {
         return true;
       }
     };
